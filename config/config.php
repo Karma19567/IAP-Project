b34c0ee87cf32bc2af71e0a60850cca11360bb2e
Ephy Macharia <ephy.macharia.mbuu@gmail.com>
2025-09-22 07:37:29 +0300

    Database configuration

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+// Database configuration
+return [
+    'host'     => 'localhost',     
+    'port'     => 3306,            
+    'dbname'   => '', 
+    'username' => 'root',
+    'password' => '',
+];
